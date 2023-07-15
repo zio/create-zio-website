d5fb93318775e8f2a127cb4d14ef6ec75553578d
Milad Khajavi <khajavi@gmail.com>
2023-07-15 17:47:16 +0330

    Revert "use await import syntax"
    This reverts commit 06a9773f3bb5e9405f04f6b729df1bbbc130d7bc.

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
-const { resolve } = await import('path');
-const { create } = await import('create-initializer');
+const { resolve } = require('path');
+const { create } = require('create-initializer');
 
 const templateRoot = resolve(__dirname, '..', 'templates');
 
